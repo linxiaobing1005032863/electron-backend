b81a5461e357e699fce936fe983a7fe64b61ecde
魑魅魍魉 <15271835932@163.com>
2017-09-20 11:55:01 +0800

    Delete config.js

--- src/config.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by chenlizan on 2017/6/21.
- */
-
-export const authServerAddress = 'http://192.168.0.12:9000';
-export const userServerAddress = 'http://192.168.0.12:9002';
-export const logServerAddress = 'http://192.168.0.12:9003';
-export const codeServerAddress = 'http://192.168.0.12:9006';
-export const goodServerAddress= 'http://192.168.0.12:9005';
